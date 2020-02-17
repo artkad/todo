5d065d2424d7f2512dc79df84aaa9f7a2f18e41b
artkad <ar_kad@bk.ru>
2020-02-17 13:16:31 +0500

    5commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello art???</h1>;
-
+const el = (
+  <div>
+    <h1>My todo List</h1>
+    <input placeholder="search"/>
+    <ul>
+      <li>Leart React</li>
+      <li>Build App</li>
+    </ul>
+  </div>
+);
 ReactDOM.render(el,
   document.getElementById('root'));
